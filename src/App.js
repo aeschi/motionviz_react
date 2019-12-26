0f03cc8f6863349f7bf69fcf5151e0f8611e224a
Anna Eschenbacher <anna.eschenbacher@icloud.com>
2019-12-26 15:31:52 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import "./App.css";
+import Title from "./components/Title.js";
+import Thing from "./components/Thing.js";
 
 function App() {
+  const test = "Hello world!";
+  const onClick = () => console.log("clicked");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title title="Ueberschrift" />
+      <p>{test}</p>
+      <button type="button" onClick={onClick}>
+        click me!
+      </button>
+      <Canvas>
+        <Thing />
+      </Canvas>
     </div>
   );
 }
